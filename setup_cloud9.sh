c0d6c9994dd9d7463b87ea9e613f26175a2ad00f
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-21 07:25:58 -0500

    new

---
+++ setup_cloud9.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install -y ipython python-matplotlib python-sklearn python-pandas htop
+
+scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_bike_sharing/*.csv .
