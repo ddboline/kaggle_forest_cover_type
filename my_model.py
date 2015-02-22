7e28bea50ccbb009d854fd404fe9bec4a0ec2721
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-22 17:57:27 -0500

    ...

--- my_model.py
+++ my_model.py
@@ -9,8 +9,55 @@ import numpy as np
 import pandas as pd
 
 from sklearn import cross_validation
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.neural_network import BernoulliRBM
+
+from sklearn.metrics import accuracy_score
+
+def gaussian(x, mu, sig):
+    return np.exp(-(x-mu)**2/(2*sig**2))/(sig*np.sqrt(2*np.pi))
+
+def fit_func(x, *p):
+    return p[2] * gaussian(x, p[0], p[1])
+
+def get_plots(in_df):
+    print in_df.columns
+    
+    for c in in_df.columns:
+        if c in ('Id', 'Cover_Type'):
+            continue
+        pl.clf()
+        nent = len(in_df[c])
+        hmin, hmax = in_df[c].min(), in_df[c].max()
+        xbins = np.linspace(hmin,hmax,nent//500)
+        for n in range(1,8):
+            covtype = in_df.Cover_Type == n
+            a = in_df[covtype][c].values
+            #b = in_df[covtype][c].hist(bins=xbins, histtype='step')
+            pl.hist(a, bins=xbins, histtype='step')
+            if c == 'Elevation':
+                mu, sig = a.mean(), a.std()
+                x = np.linspace(hmin,hmax,1000)
+                y = (a.sum()/len(xbins)) * gaussian(x, mu, sig)
+                pl.plot(x, y, '--')
+        pl.title(c)
+        pl.savefig('%s.png' % c)
+
+def make_hash(in_df):
+    tmp = in_df['Soil_Type1'].values
+    for n in range(2,40+1):
+        tmp2 = in_df['Soil_Type%s' % n].values
+        tmp |= (tmp2 << (n-1))
+    in_df['Soil_Type_Hash'] = tmp
+    cols = []
+    for n in range(1,40+1):
+        cols.append('Soil_Type%s' % n)
+    in_df = in_df.drop(labels=cols, axis=1)
+    return in_df
 
 def load_data():
     train_df = pd.read_csv('train.csv')
@@ -21,18 +68,42 @@ def load_data():
         #print df.shape
         #print '\n%s' % '\n'.join(df.columns)
     
+    #print train_df.describe()
+    
+    #get_plots(train_df)
+
+    #train_df = make_hash(train_df)
+    #print train_df['Soil_Type_Hash'].describe()
+
+    #keep_cols = ['Id', 'Cover_Type'] + ['Soil_Type%d' % n for n in range(1,41)]
+    #drop_cols = [c for c in train_df.columns if c not in keep_cols]
+    
+    #train_df = train_df.drop(labels=drop_cols, axis=1)
+
+    #print help(train_df.Aspect.quantile)
+
     #for c in train_df.columns:
         #print train_df[c].dtype, c
     
-    #print train_df.describe()
+    cols_drop = [c for c in train_df.columns if 'Soil_Type' not in c]
+    xtrain = train_df.drop(labels=cols_drop, axis=1)
+    cols_drop = [c for c in test_df.columns if 'Soil_Type' not in c]
+    xtest = test_df.drop(labels=cols_drop, axis=1)
     
+    xt = pd.concat([xtrain, xtest], axis=0)
+    
+    model = BernoulliRBM(n_components=7)
+    model.fit(xt.values)
+    train_df['rbm'] = model.predict(xtrain.values)
+    test_df['rbm'] = model.predict(xtest.values)
+    del xtrain, xtest
+
     xtrain = train_df.drop(labels=['Id','Cover_Type'], axis=1).values
     ytrain = train_df['Cover_Type'].values
     xtest = test_df.drop(labels=['Id'], axis=1).values
     ytest = ssub_df['Id'].values
     
     print xtrain.shape, ytrain.shape, xtest.shape, ytest.shape
-    print ytrain
     
     return xtrain, ytrain, xtest, ytest
 
@@ -42,6 +113,8 @@ def score_model(model, xtrain, ytrain):
                                                                      ytrain,
                                                                      test_size=0.4, random_state=randint)
     model.fit(xTrain, yTrain)
+    ytest_pred = model.predict(xTest)
+    print 'accuracy', accuracy_score(ytest_pred,yTest)
     return model.score(xTest, yTest)
 
 def prepare_submission(model, xtrain, ytrain, xtest, ytest):
@@ -58,14 +131,15 @@ def prepare_submission(model, xtrain, ytrain, xtest, ytest):
 if __name__ == '__main__':
     xtrain, ytrain, xtest, ytest = load_data()
 
-    pca = PCA()
-    pca.fit(xtrain)
+    #pca = PCA()
+    #pca.fit(xtrain)
     
-    xtrain = pca.transform(xtrain)
-    xtest = pca.transform(xtest)
+    #xtrain = pca.transform(xtrain)
+    #xtest = pca.transform(xtest)
 
-    model = RandomForestRegressor(n_jobs=-1)
-    #model = RandomForestRegressor()
+    #model = RandomForestRegressor(n_jobs=-1)
+    model = RandomForestClassifier(n_estimators=200)
+    #model = KNeighborsClassifier(7)
     print 'score', score_model(model, xtrain, ytrain)
     print model.feature_importances_
-    prepare_submission(model, xtrain, ytrain, xtest, ytest)
+    #prepare_submission(model, xtrain, ytrain, xtest, ytest)
