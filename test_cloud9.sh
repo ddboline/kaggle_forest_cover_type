7971214b27b6c8e52a49e376696d0753ac679610
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-02 22:14:39 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -4,3 +4,5 @@ rm /home/ubuntu/.ssh/known_hosts
 scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_forest_cover_type/*.csv .
 
 ./my_model.py
+
+ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk DONE"
