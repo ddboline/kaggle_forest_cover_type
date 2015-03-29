ef92c2bb3b4fe460e6fed3f0eaffe04aa828970e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-29 00:30:40 -0400

    update

--- my_model.py
+++ my_model.py
@@ -16,6 +16,8 @@ from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.naive_bayes import GaussianNB
 
+from sklearn.grid_search import RandomizedSearchCV, GridSearchCV
+
 from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.neural_network import BernoulliRBM
@@ -51,7 +53,7 @@ def create_html_page_of_plots(list_of_plots):
 def get_plots(in_df):
     list_of_plots = []
     print in_df.columns
-    
+
     for c in in_df.columns:
         if c in ('Id', 'Cover_Type'):
             continue
@@ -73,11 +75,11 @@ def get_plots(in_df):
         pl.savefig('%s.png' % c)
         list_of_plots.append('%s.png' % c)
     create_html_page_of_plots(list_of_plots)
-    
+
 def plot_failures(in_array, covertype):
     print in_array.shape
     list_of_plots = []
-    
+
     for c in range(in_array.shape[1]):
         pl.clf()
         nent = in_array.shape[0]
@@ -91,8 +93,8 @@ def plot_failures(in_array, covertype):
         pl.savefig('%s.png' % c)
         list_of_plots.append('%s.png' % c)
     create_html_page_of_plots(list_of_plots)
-        
-    
+
+
 def transform_from_classes(inp):
     y = np.zeros((inp.shape[0], 7), dtype=np.int64)
     for (index, Class) in enumerate(inp):
@@ -108,13 +110,13 @@ def load_data():
     train_df = pd.read_csv('train.csv')
     test_df = pd.read_csv('test.csv')
     ssub_df = pd.read_csv('sampleSubmission.csv')
-    
-    get_plots(train_df)
+
+    #get_plots(train_df)
 
     #xtrain = train_df.drop(labels=['Id','Cover_Type'], axis=1).values
     #ytrain = train_df['Cover_Type'].values
     #xtest = test_df.drop(labels=['Id'], axis=1).values
-    
+
     #model = KNeighborsClassifier(7)
     #model.fit(xtrain, ytrain)
     #train_df['knn'] = model.predict(xtrain)
@@ -127,94 +129,83 @@ def load_data():
                       #'Soil_Type28', 'Soil_Type34', 'Soil_Type36',
                       #]
     labels_to_drop = [
-        #u'Elevation', 
-        #u'Aspect', 
-        #u'Slope', 
-        #u'Horizontal_Distance_To_Hydrology', 
-        #u'Vertical_Distance_To_Hydrology', 
-        #u'Horizontal_Distance_To_Roadways', 
-        #u'Hillshade_9am', 
-        #u'Hillshade_Noon', 
-        #u'Hillshade_3pm', 
-        #u'Horizontal_Distance_To_Fire_Points', 
-        #u'Wilderness_Area1', 
-        #u'Wilderness_Area2', 
-        #u'Wilderness_Area3', 
-        #u'Wilderness_Area4', 
+        #u'Elevation',
+        #u'Aspect',
+        #u'Slope',
+        #u'Horizontal_Distance_To_Hydrology',
+        #u'Vertical_Distance_To_Hydrology',
+        #u'Horizontal_Distance_To_Roadways',
+        #u'Hillshade_9am',
+        #u'Hillshade_Noon',
+        #u'Hillshade_3pm',
+        #u'Horizontal_Distance_To_Fire_Points',
+        #u'Wilderness_Area1',
+        #u'Wilderness_Area2',
+        #u'Wilderness_Area3',
+        #u'Wilderness_Area4',
         #u'Soil_Type1', u'Soil_Type2', u'Soil_Type3', u'Soil_Type4', u'Soil_Type5', u'Soil_Type6', u'Soil_Type7', u'Soil_Type8', u'Soil_Type9', u'Soil_Type10', u'Soil_Type11', u'Soil_Type12', u'Soil_Type13', u'Soil_Type14', u'Soil_Type15', u'Soil_Type16', u'Soil_Type17', u'Soil_Type18', u'Soil_Type19', u'Soil_Type20', u'Soil_Type21', u'Soil_Type22', u'Soil_Type23', u'Soil_Type24', u'Soil_Type25', u'Soil_Type26', u'Soil_Type27', u'Soil_Type28', u'Soil_Type29', u'Soil_Type30', u'Soil_Type31', u'Soil_Type32', u'Soil_Type33', u'Soil_Type34', u'Soil_Type35', u'Soil_Type36', u'Soil_Type37', u'Soil_Type38', u'Soil_Type39', u'Soil_Type40'
-        ]
+       ]
 
     xtrain = train_df.drop(labels=['Id','Cover_Type']+labels_to_drop, axis=1).values
     ytrain = transform_from_classes(train_df['Cover_Type'].values)
     #ytrain = train_df['Cover_Type'].values
     xtest = test_df.drop(labels=['Id']+labels_to_drop, axis=1).values
     ytest = ssub_df['Id'].values
-    
+
     print xtrain.shape, ytrain.shape, xtest.shape, ytest.shape
-    
+
     return xtrain, ytrain, xtest, ytest
 
-def train_individual(xtrain, ytrain, xtest):
-    #ytrain_pred = np.zeros(ytrain.shape[0])
-    #ytest_pred = np.zeros((xtest.shape[0],ytrain.shape[1]))
-    #model = MiniBatchKMeans(n_clusters=7)
-    #ytest_pred = model.fit_predict(xtest)
-    #ytrain_pred = model.predict(xtrain)
-    #return np.hstack([xtrain, ytrain_pred.reshape(xtrain.shape[0],1)]), np.hstack([xtest, ytest_pred.reshape(xtest.shape[0],1)])
-    
-    pca = ProbabilisticPCA(whiten=True)
+def scorer(estimator, X, y):
+    ypred = estimator.predict(X)
+    return accuracy_score(ypred, y)
 
-    xtest = pca.fit_transform(xtest)
-    xtrain = pca.transform(xtrain)
-    
+#def train_model_parallel(model, xtrain, ytrain, index):
+    #randint = reduce(lambda x,y: x|y, [ord(x)<<(n*8) for (n,x) in enumerate(os.urandom(4))])
+    #xTrain, xTest, yTrain, yTest = \
+      #cross_validation.train_test_split(xtrain, ytrain[:,index], test_size=0.4,
+                                        #random_state=randint)
+    #n_est = [10, 100, 200]
+    #m_dep = [5, 10, 40]
 
-    return xtrain, xtest
+    #model = GridSearchCV(estimator=model,
+                                #param_grid=dict(n_estimators=n_est, max_depth=m_dep),
+                                #scoring=scorer,
+                                #n_jobs=-1, verbose=1)
+    #model.fit(xTrain, yTrain)
+    #print model
 
-def score_model(model, xtrain, ytrain):
+    #ytest_pred = model.predict(xTest)
+    #ytest_prob = model.predict_proba(xTest)
+    #print 'accuracy', accuracy_score(ytest_pred,yTest)
+    #print 'logloss', log_loss(yTest, ytest_prob)
+    #with gzip.open('model_%d.pkl.gz' % index, 'wb') as mfile:
+        #pickle.dump(model, mfile, protocol=2)
+    #return
+
+def score_model(model, xtrain, ytrain, index=0):
     randint = reduce(lambda x,y: x|y, [ord(x)<<(n*8) for (n,x) in enumerate(os.urandom(4))])
     xTrain, xTest, yTrain, yTest = cross_validation.train_test_split(xtrain,
-                                                                     ytrain,
+                                                                     ytrain[:,index],
                                                                      test_size=0.4, random_state=randint)
     model.fit(xTrain, yTrain)
     ytest_pred = model.predict(xTest)
-    
-    #print np.argmax(ytest_pred, axis=1)
-    
-    #pred_failed = ytest_pred != yTest
-    #plot_failures(xTest[pred_failed], yTest[pred_failed])
-    #print 'accuracy', accuracy_score(np.argmax(ytest_pred, axis=1), 
-                                     #np.argmax(yTest, axis=1))
-    
-    #ytest_pred = np.zeros(yTest.shape)
-    #for n in range(7):
-        #model.fit(xTrain, yTrain[:,n])
-        #ytest_pred[:,n] = model.predict(xTest)
-        #print 'accuracy', accuracy_score(ytest_pred[:,n],yTest[:,n])
-    #for x in range(ytest_pred.shape[0]):
-        #nsel = ytest_pred[x,:].sum()
-        #if nsel == 0:
-            #npick = np.random.randint(6)
-            #ytest_pred[x,npick] = 1
-        #elif nsel > 1:
-            #npick = np.random.randint(nsel)
-            #tidx = 0
-            #for n in range(6):
-                #if ytest_pred[x,n] == 1:
-                    #if tidx != npick:
-                        #ytest_pred[x,n] = 0
-                    #tidx += 1
+    ytest_prob = model.predict_proba(xTest)
+
     print 'accuracy', accuracy_score(ytest_pred,yTest)
-    
-    return model.score(xTest, yTest)
+    print 'pred', ytest_pred
+    print 'proba', ytest_prob[:,1]
+
+    return
 
 def prepare_submission(model, xtrain, ytrain, xtest, ytest):
     model.fit(xtrain, ytrain)
     ytest2 = transform_to_class(model.predict(xtest).astype(np.int64))
     #dateobj = map(datetime.datetime.fromtimestamp, ytest)
-    
+
     df = pd.DataFrame({'Id': ytest, 'Cover_Type': ytest2}, columns=('Id', 'Cover_Type'))
     df.to_csv('submission.csv', index=False)
-    
+
     return
 
 
@@ -224,15 +215,9 @@ if __name__ == '__main__':
     #model = RandomForestRegressor(n_jobs=-1)
     model = RandomForestClassifier(n_estimators=400, n_jobs=-1)
     #model = DecisionTreeClassifier()
-    #model = GradientBoostingClassifier()
-    #model = KNeighborsClassifier(7)
-    
-    #model = Pipeline([('kpca', KernelPCA(n_components=7)),
-                      #('rf', RandomForestClassifier(n_estimators=400)),])
-    
-    #model = SVC()
-    xtrain, xtest = train_individual(xtrain, ytrain, xtest)
-    
-    print 'score', score_model(model, xtrain, ytrain)
+    #model = GradientBoostingClassifier(loss='deviance', verbose=1)
+
+    train_model_parallel(model, xtrain, ytrain, 0)
+    #print 'score', score_model(model, xtrain, ytrain)
     #print model.feature_importances_
-    prepare_submission(model, xtrain, ytrain, xtest, ytest)
+    #prepare_submission(model, xtrain, ytrain, xtest, ytest)
