302a93a0d0d7f3ec5ba8fae51fb8a765785ee9b7
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-30 22:09:52 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 rm /home/ubuntu/.ssh/known_hosts
-scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_forest_cover_type/*.csv .
+scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/backup_kaggle/kaggle_forest_cover_type/forest_cover_type.tar.gz .
+tar zxvf forest_cover_type.tar.gz
+rm forest_cover_type.tar.gz
 
 ./my_model.py
 
-ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk DONE"
+ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk FOREST_COVER_DONE"
